3d23aabacc4e9fdcf99699f6942338cb087eaf9e
Alwyn Grant <marzgrant@gmail.com>
2023-05-25 20:29:30 -0700

    google map working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: "400px",
-  height: "400px",
+  width: "800px",
+  height: "800px",
 };
 
 const center = {
@@ -13,40 +12,23 @@ const center = {
   lng: -38.523,
 };
 
-function App() {
 
+function App() {
+  //isLoaded starts as false, maybe a useEffect to set true instantly???
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyAQN7cmsB0x2kvPyG-hqLQ8AJcGIKeVB4E",
   });
 
-  const [map, setMap] = React.useState(null);
-
-    const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map)
-  }, [])
-
-    const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-  
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
+  return (
+    <>
+      {isLoaded && (
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      )}
+    </>
   );
 }
 
